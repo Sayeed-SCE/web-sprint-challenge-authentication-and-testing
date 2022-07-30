a77eb12fe80c797f076088dd2eab917279d04b98
Sayeed Najeeb <48534405+Sayeed-SCE@users.noreply.github.com>
2022-07-29 19:35:58 -0700

    completed restricted.js file

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,21 @@
+const { JWT_SECRET } = require('../secrets/index')
+const jwt = require('jsonwebtoken')
+
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization
+  if (!token){
+    return next({ status: 401, message: 'Token required'})
+  }
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if(err){
+      next({status: 401, message: 'Token invalid'})
+    }else{
+      req.decodedToken = decodedToken
+      next()
+    }
+  })
+  
   /*
     IMPLEMENT
 
